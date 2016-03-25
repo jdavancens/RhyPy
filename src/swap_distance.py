12e14114de71f5f811733a3be71abe716c60c613
jdavancens <josephdavancens@gmail.com>
2016-03-24 17:55:57 -0700

    Docs cleanup

--- src/swap_distance.py
+++ src/swap_distance.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+
 def swap_distance(self, A, B):
     r''' A special instance of the "earth mover's distance," which measures
-    the dissimilarity of two equal length rhythms.
+    the dissimilarity of two equal length rhythms. Basically, the sum of the
+    absolute values of the differences between the elements of A and B.
     '''
     from rhypy import Rhythm
-    assert(isinstance(A, Rhythm) and isinstance(B, Rhythm))
-    assert(len(A) == len(B))
+    assert isinstance(A, Rhythm) and isinstance(B, Rhythm) , \
+        "Both parameters must be of type Rhythm"
+
+    assert len(A) == len(B) , "Rhythms must be the same length"
     return sum([abs(a-b) for a,b in zip(A,B)])
